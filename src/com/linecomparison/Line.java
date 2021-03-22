789c8e1db4e5cf1d65f38f1fc01eaaedf434d0a3
stutiid <stutichaturvedi12@gmail.com>
2021-03-22 20:24:33 +0530

    welcom to line comparison computation

---
+++ src/com/linecomparison/Line.java
@@ -0,0 +1,8 @@
+package com.linecomparison;
+
+public class Line {
+public static void main(String args[]) {
+	//welcome message
+	System.out.println("Welcome to Line Comparison Computation");
+}
+}
