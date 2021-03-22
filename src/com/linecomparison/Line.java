61af59d721dbf0873624e32894f9bac80d327598
stutiid <stutichaturvedi12@gmail.com>
2021-03-22 20:43:11 +0530

    Added: calculated length of line

--- src/com/linecomparison/Line.java
+++ src/com/linecomparison/Line.java
@@ -1,8 +1,24 @@
 package com.linecomparison;
 
+import java.util.Scanner;
+
 public class Line {
 public static void main(String args[]) {
 	//welcome message
 	System.out.println("Welcome to Line Comparison Computation");
+	
+	//variables
+	double x1,y1,x2,y2; //line coordinates
+	double lineLength;
+	//taking input from user
+	System.out.println("enter the line coordinates");
+	Scanner s= new Scanner(System.in);
+	x1=s.nextDouble();
+	y1=s.nextDouble();
+	x2=s.nextDouble();
+	y2=s.nextDouble();
+	//computation
+	lineLength= Math.sqrt(Math.pow((x2-x1),2)+Math.pow(y2-y1,2));
+	System.out.println("Line length is "+lineLength);
 }
 }
