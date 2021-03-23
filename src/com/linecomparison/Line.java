ccb267f5d090b38dcb4ea0db04e334f44f3fa2ed
stutiid <stutichaturvedi12@gmail.com>
2021-03-23 20:19:14 +0530

    Added:calculating whether the two lines are of equal length or not

--- src/com/linecomparison/Line.java
+++ src/com/linecomparison/Line.java
@@ -3,32 +3,33 @@ package com.linecomparison;
 import java.util.Scanner;
 
 public class Line {
-public static void main(String args[]) {
-	//welcome message
-	System.out.println("Welcome to Line Comparison Computation");
-	
-	//variables
-	double x1,y1,x2,y2; //line coordinates
-	double lineLength1,lineLength2;
-	//taking input from user
-	System.out.println("enter the line coordinates for line1");
-	Scanner s= new Scanner(System.in);
-	x1=s.nextDouble();
-	y1=s.nextDouble();
-	x2=s.nextDouble();
-	y2=s.nextDouble();
-	//computation
-	lineLength1= Math.sqrt(Math.pow((x2-x1),2)+Math.pow(y2-y1,2));
-	System.out.println("enter the line coordinates for line1");
-	x1=s.nextDouble();
-	y1=s.nextDouble();
-	x2=s.nextDouble();
-	y2=s.nextDouble();
-	lineLength2= Math.sqrt(Math.pow((x2-x1),2)+Math.pow(y2-y1,2));
-	//converting Primitive to object
-	Double Line1=Double.valueOf(lineLength1);
-	Double Line2=Double.valueOf(lineLength2);
-	boolean T=Line1.equals(Line2);
-	System.out.println("Two lines are equals: "+T);
-}
+	public static void main(String args[]) {
+		// welcome message
+		System.out.println("Welcome to Line Comparison Computation");
+
+		// variables
+		double x1, y1, x2, y2; // line coordinates
+		double lineLength1, lineLength2;
+		// taking input from user
+		System.out.println("enter the line coordinates for line1");
+		Scanner scanner = new Scanner(System.in);
+		x1 = scanner.nextDouble();
+		y1 = scanner.nextDouble();
+		x2 = scanner.nextDouble();
+		y2 = scanner.nextDouble();
+		// calculating length of line1
+		lineLength1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow(y2 - y1, 2));
+		System.out.println("enter the line coordinates for line1");
+		x1 = scanner.nextDouble();
+		y1 = scanner.nextDouble();
+		x2 = scanner.nextDouble();
+		y2 = scanner.nextDouble();
+		// calculating length of line2
+		lineLength2 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow(y2 - y1, 2));
+		// converting Primitive to object
+		Double lengthLine1 = Double.valueOf(lineLength1);
+		Double lengthLine2 = Double.valueOf(lineLength2);
+		boolean equalityCheck = lengthLine1.equals(lengthLine2);
+		System.out.println("Two lines are equals: " + equalityCheck);
+	}
 }
